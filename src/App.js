aa7115ce03932992984c4e99d9b65ba59a6300fd
Djordje Knezevic <starwarrior@mycity.rs>
2023-03-31 21:23:17 +0200

    Send get request to Deezer API via Heroku app, save response and output album title, release date, cover and songs list

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import "./App.css";
 
 function App() {
   const [album, setAlbum] = useState({});
+  const [songs, setSongs] = useState();
 
   const fetchAlbumDataHandler = async () => {
     try {
-      const response = await fetch('https://api.deezer.com/album/93733532');
-      // const response = await fetch("https://swapi.dev/api/films/1/");
+      const response = await fetch(
+        "https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/93733532"
+      );
       const albumData = await response.json();
+
+      const songsList = albumData.tracks.data.map((song) => {
+        return <div>{song.title}</div>;
+      });
       setAlbum(albumData);
+      setSongs(songsList);
       console.log(albumData);
     } catch (error) {
       console.log(error);
     }
   };
+
   return (
     <div className="App">
       <h1>Sneezer</h1>
       <p>Nose around that Deezer music library!</p>
       <button onClick={fetchAlbumDataHandler}>Get album data</button>
       <div>{album.title}</div>
+      <div>{album.release_date}</div>
+      <img src={album.cover_medium} alt="" />
+      <div>{songs}</div>
     </div>
   );
 }
