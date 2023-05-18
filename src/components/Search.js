82608769cae0bc60ae43886a80b101b4e90a075c
Djordje Knezevic <starwarrior@mycity.rs>
2023-05-18 21:24:06 +0200

    Add logic for switching search types between tracks, albums and artists

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,41 +1,77 @@
-import React, { useState, useEffect } from "react";
-import SongsList from "./SongsList";
+import React, { useEffect, useReducer } from 'react';
+import SongsList from './SongsList';
 import LoadingSpinner from './LoadingSpinner';
 import useHttp from '../hooks/use-http';
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+const defaultSearchState = {
+  searchType: 'track',
+  searchTerm: '',
+};
 
-  const { isLoading, sendRequest: fetchSongs, results, setResults } = useHttp();
+const searchReducer = (state, action) => {
+  if (action.type === 'INPUT') {
+    return { searchType: state.searchType, searchTerm: action.userInput };
+  }
+  if (action.type === 'SEARCH_TRACKS') {
+    return { searchType: 'track', searchTerm: state.searchTerm };
+  }
+  if (action.type === 'SEARCH_ALBUMS') {
+    return { searchType: 'album', searchTerm: state.searchTerm };
+  }
+  if (action.type === 'SEARCH_ARTISTS') {
+    return { searchType: 'artist', searchTerm: state.searchTerm };
+  }
+
+  return defaultSearchState;
+};
+
+const Search = () => {
+  const [searchState, dispatch] = useReducer(searchReducer, defaultSearchState);
+  const { isLoading, sendRequest: fetchData, results, setResults } = useHttp();
+
+  const searchUrl = `https://api.deezer.com/search/${searchState.searchType}?q=${searchState.searchTerm}`;
 
   const searchInputHandler = (event) => {
-    setSearchTerm(event.target.value);
+    dispatch({ type: 'INPUT', userInput: event.target.value });
+  };
+
+  const tracksPressedHandler = (event) => {
+    dispatch({ type: 'SEARCH_TRACKS' });
+  };
+
+  const albumsPressedHandler = (event) => {
+    dispatch({ type: 'SEARCH_ALBUMS' });
+  };
+
+  const artistPressedHandler = (event) => {
+    dispatch({ type: 'SEARCH_ARTISTS' });
   };
 
   useEffect(() => {
-    if (searchTerm === '') {
+    if (searchState.searchTerm.trim() === '') {
       setResults(null);
       console.log('Empty search term!');
       return;
     }
 
     const timer = setTimeout(() => {
-      fetchSongs({
-        url: `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${searchTerm}`,
+      fetchData({
+        url: `https://cors-anywhere.herokuapp.com/${searchUrl}`
       });
+      console.log(searchUrl);
     }, 500);
 
     return () => clearTimeout(timer);
-  }, [searchTerm, fetchSongs, setResults]);
+  }, [searchUrl, searchState.searchTerm, fetchData, setResults]);
 
   const loadPreviousHandler = () => {
-    fetchSongs({
+    fetchData({
       url: `https://cors-anywhere.herokuapp.com/${results.prev}`,
     });
   };
 
   const loadNextHandler = () => {
-    fetchSongs({
+    fetchData({
       url: `https://cors-anywhere.herokuapp.com/${results.next}`,
     });
   };
@@ -43,16 +79,21 @@ const Search = () => {
   return (
     <React.Fragment>
       <div>
+        <button onClick={tracksPressedHandler}>Tracks</button>
+        <button onClick={albumsPressedHandler}>Albums</button>
+        <button onClick={artistPressedHandler}>Artists</button>
+      </div>
+      <div>
         <input type='text' onChange={searchInputHandler} />
       </div>
       {isLoading && <LoadingSpinner />}
-      {!isLoading && searchTerm !== '' && results && (
+      {!isLoading && searchState.searchTerm !== '' && searchState.searchType === 'track' && results && (
         <SongsList songs={results.data} />
       )}
-      {!isLoading && searchTerm !== '' && results && results.prev && (
+      {!isLoading && searchState.searchTerm !== '' && results && results.prev && (
         <button onClick={loadPreviousHandler}>Previous</button>
       )}
-      {!isLoading && searchTerm !== '' && results && results.next && (
+      {!isLoading && searchState.searchTerm !== '' && results && results.next && (
         <button onClick={loadNextHandler}>Next</button>
       )}
     </React.Fragment>
